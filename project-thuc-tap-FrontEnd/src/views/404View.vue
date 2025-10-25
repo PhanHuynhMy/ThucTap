e44c1d096727325ac68083ffc38a4a866e2f4c85
PhanHuynhMy <myb2000142@student.ctu.edu.vn>
2025-10-26 01:27:07 +0700

    projectt

---
+++ project-thuc-tap-FrontEnd/src/views/404View.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <SinglePageHeaderVue />
+    <PageFound />
+    <AppFooter />
+    <Login />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/User/layout/NavBar.vue";
+import AppFooter from "@/components/User/layout/AppFooter.vue";
+import PageFound from "../components/User/404/Page404.vue";
+import SinglePageHeaderVue from "@/components/User/404/SinglePageHeader.vue";
+export default {
+  name: "404View",
+  components: { NavBar, PageFound, SinglePageHeaderVue, AppFooter, }, // Đảm bảo chỉ sử dụng component Login ở đây
+};
+</script>
+
+<style></style>
